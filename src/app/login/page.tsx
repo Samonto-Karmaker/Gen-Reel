2a34ec5931395e91e43574fd65c17b51b5da6333
Samanta Karmaker <samonto0726@gmail.com>
2025-03-06 21:33:47 +0600

    Implement initial login form with email and password handling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,25 @@
+"use client"
+
 import Link from "next/link"
+import { FormEvent, useState } from "react"
+import { useNotification } from "../components/Notification"
 
 export default function Login() {
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const { showNotification } = useNotification()
+
+	const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		showNotification(`${email} ${password}`, "info")
+	}
+
 	return (
 		<div className="max-w-md mx-auto">
 			<div className="card w-100 bg-base-100 shadow-lg">
 				<div className="card-body">
 					<h1 className="text-3xl font-bold mb-4">Login</h1>
-					<form className="space-y-4">
+					<form className="space-y-4" onSubmit={handleLogin}>
 						<div>
 							<label htmlFor="email" className="block mb-2 text-lg font-medium">
 								Email
@@ -14,6 +27,8 @@ export default function Login() {
 							<input
 								className="w-full px-3 py-2 border-2 border-black rounded-lg"
 								type="email"
+								value={email}
+								onChange={e => setEmail(e.target.value)}
 								required
 								placeholder="mail@site.com"
 							/>
@@ -28,6 +43,8 @@ export default function Login() {
 							<input
 								className="w-full px-3 py-2 border-2 border-black rounded-lg"
 								type="password"
+								value={password}
+								onChange={e => setPassword(e.target.value)}
 								required
 								placeholder="********"
 							/>
