fddc8965c35fb4d7316e29baa3a23f335870c48c
Samanta Karmaker <samonto0726@gmail.com>
2025-03-10 22:16:28 +0600

    implement video upload form initial UI

--- src/app/components/VideoUploadForm.tsx
+++ src/app/components/VideoUploadForm.tsx
@@ -1,13 +1,43 @@
 "use client"
 
+import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props"
+import FileUploader from "./FileUploader"
+import { useNotification } from "./Notification"
+
 export default function VideoUploadForm() {
-    return (
-        <div>
-            <h2>Upload a video</h2>
-            <form>
-                <input type="file" accept="video/*" />
-                <button type="submit">Upload</button>
-            </form>
-        </div>
-    )
+	const { showNotification } = useNotification()
+
+	const handleUploadSuccess = (response: IKUploadResponse) => {
+		showNotification("Video uploaded successfully.", "success")
+	}
+
+	const handleUploadProgress = (progress: number) => {}
+
+	return (
+		<form className="space-y-4">
+			<div className="form-control">
+				<label className="label">Title</label>
+				<input type="text" className="input input-bordered" />
+			</div>
+
+			<div className="form-control">
+				<label className="label">Description</label>
+				<input type="text" className="input input-bordered" />
+			</div>
+
+			<div className="form-control">
+				<label className="label">Upload Video</label>
+				<FileUploader
+					fileType="video"
+					onSuccess={handleUploadSuccess}
+					onProgress={handleUploadProgress}
+				/>
+			</div>
+
+            <br className="my-8" />
+			<button type="submit" className="btn btn-primary btn-block">
+				Upload
+			</button>
+		</form>
+	)
 }
