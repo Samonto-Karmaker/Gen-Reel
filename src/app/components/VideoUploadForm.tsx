23b5f967a94d41072c1ac2cebf3144574bce163a
Samanta Karmaker <samonto0726@gmail.com>
2025-03-10 22:39:34 +0600

    Integrate react-hook-form to video upload form

--- src/app/components/VideoUploadForm.tsx
+++ src/app/components/VideoUploadForm.tsx
@@ -3,9 +3,30 @@
 import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props"
 import FileUploader from "./FileUploader"
 import { useNotification } from "./Notification"
+import { useForm } from "react-hook-form"
+
+interface IVideoFormData {
+	title: string
+	description: string
+	videoUrl: string
+	thumbnailUrl: string
+}
 
 export default function VideoUploadForm() {
 	const { showNotification } = useNotification()
+	const {
+		register,
+		handleSubmit,
+		setValue,
+		formState: { errors },
+	} = useForm<IVideoFormData>({
+		defaultValues: {
+			title: "",
+			description: "",
+			videoUrl: "",
+			thumbnailUrl: "",
+		},
+	})
 
 	const handleUploadSuccess = (response: IKUploadResponse) => {
 		showNotification("Video uploaded successfully.", "success")
@@ -13,16 +34,43 @@ export default function VideoUploadForm() {
 
 	const handleUploadProgress = (progress: number) => {}
 
+	const onSubmit = (data: IVideoFormData) => {
+		showNotification("Video uploaded successfully.", "success")
+		console.log(data)
+	}
+
 	return (
-		<form className="space-y-4">
+		<form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
 			<div className="form-control">
 				<label className="label">Title</label>
-				<input type="text" className="input input-bordered" />
+				<input
+					type="text"
+					className={`input input-bordered ${
+						errors.title ? "input-error" : ""
+					}`}
+					{...register("title", { required: "Title is required" })}
+				/>
+				{errors.title && (
+					<span className="text-error text-sm mt-1">
+						{errors.title.message}
+					</span>
+				)}
 			</div>
 
 			<div className="form-control">
 				<label className="label">Description</label>
-				<input type="text" className="input input-bordered" />
+				<input
+					type="text"
+					className={`input input-bordered ${
+						errors.description ? "input-error" : ""
+					}`}
+					{...register("description", { required: "Description is required" })}
+				/>
+                {errors.description && (
+					<span className="text-error text-sm mt-1">
+						{errors.description.message}
+					</span>
+				)}
 			</div>
 
 			<div className="form-control">
@@ -34,7 +82,7 @@ export default function VideoUploadForm() {
 				/>
 			</div>
 
-            <br className="my-8" />
+			<br className="my-8" />
 			<button type="submit" className="btn btn-primary btn-block">
 				Upload
 			</button>
